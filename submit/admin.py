356fdc92b8096014fd943853bd916070131c3130
Zoltan Onody <zoltan.onody@gmail.com>
2017-01-28 15:08:23 +0100

    Show timestamp and filter by message in submit admin

--- submit/admin.py
+++ submit/admin.py
@@ -4,8 +4,8 @@ from submit.models import Submit
 
 
 class SubmitAdmin(admin.ModelAdmin):
-    list_display = ['user', 'task', 'message', 'language']
+    list_display = ['user', 'task', 'message', 'language', 'timestamp']
     search_fields = ['user__username', 'task__id', 'task__task_set__id']
-    list_filter = ['language']
+    list_filter = ['language', 'message']
 
 admin.site.register(Submit, SubmitAdmin)
